ec810667a60e45c322d0fc6bc91d8bf3eda6b880
Tejashri Rajendra Chouke <139746868+TejashriChouke@users.noreply.github.com>
2024-04-07 12:36:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+ document.getElementById('color-form').addEventListener('submit', function(event) {
+            event.preventDefault();
+            const blockId = document.getElementById('block_id').value;
+            const color = document.getElementById('colour_id').value;
+
+            const gridItem = document.getElementById(blockId);
+            if (gridItem) {
+                document.querySelectorAll('.grid-item').forEach(item => {
+                    item.style.backgroundColor = 'transparent';
+                });
+                gridItem.style.backgroundColor = color;
+            } else {
+                alert('Invalid block ID!');
+            }
+        });
+
+        document.getElementById('reset_button').addEventListener('click', function() {
+            document.querySelectorAll('.grid-item').forEach(item => {
+                item.style.backgroundColor = 'transparent';
+            });
+        });
